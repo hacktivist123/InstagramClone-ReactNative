a6b36659239680759d13abf00945e58a467cfcfc
hacktivist123 <akintayoshedrack@gmail.com>
2017-11-23 17:43:35 +0100

    updated A few things, App.js

--- instagramclone-reactnative/App.js
+++ instagramclone-reactnative/App.js
@@ -1,21 +1,110 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import { Text, View, ImageBackground, StatusBar, ScrollView, Image} from 'react-native';
+import LoginButton from './src/Component/LoginButton';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Yeah our App is running!</Text>
-      </View>
-    );
-  }
+
+
+const colors = {
+  facebook: 'rgb(59, 89, 152)',
+  text: 'rgba(255, 255, 255, 0.75)',
+  instagramButtonBorderColor: 'rgba(255, 255, 255, 0.35)',
+  facebookButtonBorderColor: 'rgba(255, 255, 255, 0.35)'
 }
 
-const styles = StyleSheet.create({
+const loginButtonInfo = {
+  height: 45,
+  pageFontSize: 11,
+  borderWidth: 0.8,
+  borderRadius: 5
+}
+
+const urls = {
+  forgotInstagramLogin: 'https://www.instagram.com/accounts/password/reset',
+  twitterLogin: 'https://twitter.com/login?lang=en',
+  instagramSignUp: 'https://www.instagram.com/accounts/emailsignup/?hl=en',
+  instagramAuthLogin: 'https://api.instagram.com/oauth/authorize/?client_id=cda6dee7d8164a868150910407962f52&redirect_uri=http://www.kaitechconsulting.com&response_type=token&scope=basic+follower_list+comments+likes',
+  instagramLogout: 'https://instagram.com/accounts/logout',
+  instagramBase: 'https://www.instagram.com/',
+};
+
+
+export default class App extends Component {
+
+  constructor(props){
+    super(props);
+  }
+
+  loginButtonPressed = () => {
+    console.log("Button was pressed");
+  }
+
+
+  loginScreenComponent = () => {
+    return (
+      <ImageBackground
+      source={require('./src/images/ibiza-sunset')}
+      resizeMode={'cover'} style={viewStyles.container}
+      >
+      <StatusBar
+      baclgroundColor="gray"
+      barStyle="light-content"
+      />
+
+      <ScrollView>
+
+      <Image
+      source={require('./src/images/instagram-text-black.png')}
+      style={viewStyles.instagramTextLogo}
+      resizeMode={'contian'}
+      />
+      <LoginButton
+      buttonViewStyle={viewStyles.instagramLoginButtonView}
+      buttonTextStyle={{color: colors.text, fontWeight: '500'}}
+      buttonTapped={this.loginButtonPressed}
+      activeOpacity={0.75}
+      >
+      Login
+
+      </LoginButton>
+
+      </ScrollView>
+
+      </ImageBackground>
+
+    );
+
+  }
+  render() {
+    return (
+      this.loginScreenComponent()
+    );
+  }
+
+}
+
+const viewStyles = {
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
   },
-});
+  instagramTextLogo: {
+    width: 150,
+    height: 80,
+    marginTop: '65%',
+    marginBottom: 25
+  },
+  instagramLoginButtonView: {
+    backgroundColor: 'transparent',
+    borderColor: colors.instagramButtonBorderColor,
+    borderWidth: loginButtonInfo.borderWidth,
+    borderRadius: loginButtonInfo.borderRadius,
+    width: '80%',
+    height: loginButtonInfo.height,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+};
+const textStyles = {
+
+};
