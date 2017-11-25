30a08ecdcdacf6433716b44538327f5696a3d0a5
hacktivist123 <akintayoshedrack@gmail.com>
2017-11-25 14:52:54 +0100

    Created an Access Token Detector

--- instagramclone-reactnative/App.js
+++ instagramclone-reactnative/App.js
@@ -1,15 +1,16 @@
 //Created by Akintayo shedrack
-//With React native
-// started ; Nov 20
+//Instagram Clone With React native
+// started ; Nov 20 2017
 
 import React, {Component} from 'react';
-import { Text, View, ImageBackground, StatusBar, ScrollView, Image, Linking} from 'react-native';
+import { Text, View, ImageBackground, StatusBar, ScrollView, Image, Linking, WebView} from 'react-native';
 import LoginButton from './src/components/LoginButton';
 import Dimensions from 'Dimensions';
 import TapableText from  './src/components/TapableText';
 
 const windowSize = Dimensions.get('window');
 const standardComponentWidth = windowSize.width*0.82;
+const twitterIconSize = 19;
 
 const colors = {
   facebook: 'rgb(59, 89, 152)',
@@ -29,7 +30,7 @@ const urls = {
   forgotInstagramLogin: 'https://www.instagram.com/accounts/password/reset',
   twitterLogin: 'https://twitter.com/login?lang=en',
   instagramSignUp: 'https://www.instagram.com/accounts/emailsignup/?hl=en',
-  instagramAuthLogin: 'https://api.instagram.com/oauth/authorize/?client_id=cda6dee7d8164a868150910407962f52&redirect_uri=http://www.kaitechconsulting.com&response_type=token&scope=basic+follower_list+comments+likes',
+  instagramAuthLogin: 'https://api.instagram.com/oauth/authorize/?client_id=d86c7691bb3a41f9a6f24c21276a04ba&redirect_uri=https://hacktivist123.github.io/Akintayoshedrack.me&response_type=token&scope=basic+follower_list+comments+likes',
   instagramLogout: 'https://instagram.com/accounts/logout',
   instagramBase: 'https://www.instagram.com/',
 };
@@ -39,12 +40,67 @@ export default class App extends Component {
 
   constructor(props){
     super(props);
+
+    this.state = {
+      authenticationURL:urls.instagramAuthLogin,
+      accessToken: '',
+      isUserLoggedIn: false,
+      displayAuthenticationWebView: false,
+    }
   }
 
   loginButtonPressed = () => {
-    console.log("Button was pressed");
-  };
+    this.setState({ displayAuthenticationWebView: true});
+  }
 
+  onURLStateChange = (webViewState) => {
+    const accessTokenSubString = 'access_token='
+    console.log('Current URL = ' + webViewState.url)
+
+  if(webViewState.url.includes(accessTokenSubString)){
+    //safegaurd conditional if statement
+    if(this.state.accessToken.length < 1){
+      //the index of the begining of the accessToken
+      var startIndexOfAccessToken = webViewState.url.lastIndexOf(accessTokenSubString) + accessTokenSubString.length;
+      var foundAccessToken = webViewState.substr(startIndexOfAccessToken);
+      console.log("My Access Token = " + foundAccessToken);
+    }
+  }
+}
+  authenticationWebViewComponent = () => {
+    return(
+      <WebView source={{ uri: this.state.authenticationURL}}
+      startInLoadingState={true}
+      onNavigationStateChange={this.onURLStateChange}/>
+    );
+
+  }
+
+  loginWithTwitter = () => {
+    return (
+      <View style= {viewStyles.twitterLoginViewStyle}>
+      <Image
+      source={require('./src/images/icons/twitter_bird.png')}
+      style ={viewStyles.twitterIconViewStyle}
+      resizeMode = 'contain'/>
+      </View>
+    );
+  }
+
+  signUpFootercomponent=() => {
+    return(
+      <View style= {[viewStyles.signUpFootercomponent]}>
+      <Text style={textStyles.forgottenLogin}>Dont Have An Account?</Text>
+      <TapableText
+      textStyle={[textStyles.forgottenLogin, textStyles.forgottenLoginBold]}
+      //this should create a link behind the SignUp in text using Linkin and openURL
+      textTapped={ () => Linking.openURL(urls.instagramSignUp) }
+      >
+      Sign Up
+      </TapableText>
+      </View>
+    );
+  }
 
   loginScreenComponent = () => {
     return (
@@ -53,7 +109,7 @@ export default class App extends Component {
       resizeMode={'cover'} style={viewStyles.container}
       >
       <StatusBar
-      baclgroundColor="gray"
+      backgroundColor="gray"
       barStyle="light-content"
       />
 
@@ -95,98 +151,134 @@ export default class App extends Component {
       </TapableText>
       </View>
 
-      <view style = {viewStyles.orSeperatorView}>
-      <view style = { viewStyles.orSeperatorLine}/>
+      <View style = {viewStyles.orSeperatorView}>
+      <View style = { viewStyles.orSeperatorLine}/>
       <Text style = {textStyles.orSeperatorTextStyle}>OR</Text>
+      <TapableText
+      textStyle={[textStyles.forgottenLogin, textStyles.forgottenLoginBold, textStyles.twitterTextSize]}
+      //this should create a link behind the login with twitter in text using Linkin and openURL
+      textTapped={ () => Linking.openURL(urls.twitterLogin) }
+      >
+      Login with Twitter
+      </TapableText>
+      <View style = {viewStyles.orSeperatorLine}/>
+      </View>
 
-      </view>
+      {this.loginWithTwitter()}
 
       </ScrollView>
-
+      {this.signUpFootercomponent}
       </ImageBackground>
 
     );
 
   }
   render() {
-    return (
-      this.loginScreenComponent()
-    );
+    if (this.state.displayAuthenticationWebView == true){
+      return(
+        this.authenticationWebViewComponent()
+      );
+    }
+    else {
+      return(
+        this.loginScreenComponent()
+      );
+    }
   }
 
-}
-
-const viewStyles = {
-  container: {
-    flex: 1,
-    alignItems: 'center',
-  },
-  instagramTextLogo: {
-    width: 150,
-    height: 80,
-    marginTop: '65%',
-    marginBottom: 25,
-    alignSelf : 'center'
-  },
-  instagramLoginButtonView: {
-    backgroundColor: 'transparent',
-    borderColor: colors.instagramButtonBorderColor,
-    borderWidth: loginButtonInfo.borderWidth,
-    borderRadius: loginButtonInfo.borderRadius,
-    width: standardComponentWidth,
-    height: loginButtonInfo.height,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  instagramButtonTouchableHighlightstyle: {
-    backgroundColor: 'transparent',
-    width: standardComponentWidth,
-    height: loginButtonInfo.height,
-    marginTop: 5
-  },
-  facebookLoginButton: {
-    backgroundColor: colors.facebook,
-  },
-  facebookButtonTouchableHighlightStyle: {
-    marginTop: 20,
-    marginBottom: 5
-  },
-  forgottenLoginEncapsulationView: {
-    flexDirection: 'row',
-    flex: 1,
-    marginTop: 10,
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  orSeperatorView: {
-    flexDirection: 'row',
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: 13,
-    paddingHorizontal: 5
-  },
-  orSeperatorLine: {
-    backgroundColor: colors.instagramButtonBorderColor,
-    borderColor: colors.instagramButtonBorderColor,
-    height: 1,
-    flex: 5,
-    borderWidth: 0.5
-  },
-};
-const textStyles = {
-  forgottenLogin: {
-    color : 'white',
-    fontSize: loginButtonInfo.pageFontSize,
-    backgroundColor: 'transparent'
-  },
-  forgottenLoginBold: {
-    fontWeight: 'bold',
-    marginLeft: 3
-  },
-  orSeperatorTextStyle: {
-    color: 'white',
-    backgroundColor: 'transparent',
-    fontWeight: 'bold'
-  },
-};
+  const textStyles = {
+    forgottenLogin: {
+      color : 'white',
+      fontSize: loginButtonInfo.pageFontSize,
+      backgroundColor: 'transparent'
+    },
+    forgottenLoginBold: {
+      fontWeight: 'bold',
+      marginLeft: 3
+    },
+    orSeperatorTextStyle: {
+      color: 'white',
+      backgroundColor: 'transparent',
+      fontWeight: 'bold'
+    },
+  };
+  =======
+  const viewStyles = {
+    container: {
+      flex: 1,
+      alignItems: 'center',
+    },
+    instagramTextLogo: {
+      width: 150,
+      height: 80,
+      marginTop: '35%',
+      marginBottom: 25,
+      alignSelf : 'center'
+    },
+    instagramLoginButtonView: {
+      backgroundColor: 'transparent',
+      borderColor: colors.instagramButtonBorderColor,
+      borderWidth: loginButtonInfo.borderWidth,
+      borderRadius: loginButtonInfo.borderRadius,
+      width: standardComponentWidth,
+      height: loginButtonInfo.height,
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    instagramButtonTouchableHighlightstyle: {
+      backgroundColor: 'transparent',
+      width: standardComponentWidth,
+      height: loginButtonInfo.height,
+      marginTop: 5
+    },
+    facebookLoginButton: {
+      backgroundColor: colors.facebook,
+    },
+    facebookButtonTouchableHighlightStyle: {
+      marginTop: 20,
+      marginBottom: 5
+    },
+    forgottenLoginEncapsulationView: {
+      flexDirection: 'row',
+      flex: 1,
+      marginTop: 10,
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    orSeperatorView: {
+      flexDirection: 'row',
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      marginVertical: 14,
+      paddingHorizontal: 5
+    },
+    orSeperatorLine: {
+      backgroundColor: colors.instagramButtonBorderColor,
+      borderColor: colors.instagramButtonBorderColor,
+      height: 1,
+      flex: 5,
+      borderWidth: 0.5
+    },
+    twitterLoginViewStyle: {
+      flexDirection: 'row',
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    twitterIconViewStyle:{
+      width: twitterIconSize,
+      height: twitterIconSize,
+      MarginHorizontal: 14
+    },
+    signUpFootercomponent:{
+      flex: 0.3,
+      backgroundColor: 'rgba(255, 255, 255, 0.15)',
+      borderWidth: 1,
+      borderColor: 'rgba(255, 255, 255, 255, 0.15)',
+      shadowColor: 'black',
+      shadowOffset: {width: 0, height: 1.5},
+      height: null,
+      width: windowSize.width
+    },
+  };
